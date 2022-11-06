f51b2f164a942dc172765fe372ac8eb5a6cb39f3
Price Comstock <price.comstock@gmail.com>
2022-11-06 14:30:25 -0500

    hitbox and hurtbox

--- scenes/Hurtbox.gd
+++ scenes/Hurtbox.gd
@@ -1,21 +1,32 @@
+class_name HurtBox
 extends Area2D
 
-signal hit
-
-
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _init():
+  collision_layer = 0
+  collision_mask = 2
 
+func _ready() -> void:
+  connect("area_entered", self, "_on_area_entered")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+# By specifying the type, we get null if the object isn't a HitBox
+func _on_area_entered(hitbox: HitBox) -> void:
+  
+  if hitbox == null:
+	  return
 
-func _on_Hurtbox_body_entered(body):
-	emit_signal("hit")
-	print("hit")
-	
-	# Disables collision later on in the frame update
-	$CollisionShape2D.set_deferred("disabled", true)
+  # owner is the root of the scene
+  if owner.has_method("take_damage"):
+	  owner.take_damage(hitbox.damage)
+  
+  print("hit")
+  emit_signal("hit")
+
+
+  
+  # Disables collision later on in the frame update
+  $CollisionShape2D.set_deferred("disabled", true)
