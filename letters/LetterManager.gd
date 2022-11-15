346f94a57b903b817d6d38d76c91bb0f7508501c
Price <price.comstock@gmail.com>
2022-11-14 23:13:46 -0500

    dialogue? letter manager?

--- letters/LetterManager.gd
+++ letters/LetterManager.gd
@@ -5,15 +5,15 @@ signal letters_updated(letters_list)
 var letters_list : Array = [] setget ,get_letters
 
 func _ready():
-	letters_list = []
+  letters_list = []
 
 func get_letters() -> Array:
-	return letters_list
+  return letters_list
 
 func clear():
-	letters_list = []
-	emit_signal("letters_updated", letters_list)
+  letters_list = []
+  emit_signal("letters_updated", letters_list)
 
 func add_letter(letter: Letter):
-	letters_list.append(letter)
-	emit_signal("letters_updated", letters_list)
+  letters_list.append(letter)
+  emit_signal("letters_updated", letters_list)
