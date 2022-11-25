5941ae3f3068c59a06eefdd0e5bc80ebd66e13dc
Price <price.comstock@gmail.com>
2022-11-25 09:22:07 -0500

    add speech bubble to corpse

--- Corpse.gd
+++ Corpse.gd
@@ -1,9 +1,16 @@
 extends Node2D
 
+export var talk_delay := 1.0
+export var talk_duration := 2.0
+
 onready var remote_transform: RemoteTransform2D = $RemoteTransform2D
+onready var speech_bubble = $SpeechBubble
 
 func _ready():
   print(remote_transform)
+  speech_bubble.hide()
+
+  get_tree().create_timer(talk_delay).connect("timeout", self, "talk")
 
 func take_camera():
   remote_transform.remote_path = CameraManager.claim_camera().get_path()
@@ -11,3 +18,8 @@ func take_camera():
 
 func release_camera():
   remote_transform.remote_path = ""
+
+func talk():
+  speech_bubble.show()
+  speech_bubble.reveal_text()
+  speech_bubble.set_fade_timer(talk_duration)
--- dialogue/SpeechBubble.gd
+++ dialogue/SpeechBubble.gd
@@ -1,9 +1,27 @@
 extends Node2D
 
+# Start with text hidden and play reveal animation immediately when node is instantiated
+export var auto_reveal := true
+export var auto_fade := true
+export var fade_after_seconds := 3.0
+
 onready var label : Label = get_node("%BubbleText")
+onready var animation_player : AnimationPlayer = $AnimationPlayer
 
 func _ready():
-  label.text = "oop"
+  if (auto_reveal):
+    reveal_text()
+  else:
+    label.set_percent_visible(1.0)
+
+  if (auto_fade):
+    set_fade_timer(fade_after_seconds)
+
+func set_fade_timer(seconds):
+  get_tree().create_timer(seconds).connect("timeout", self, "hide", [], CONNECT_ONESHOT)
 
 func set_text(s: String):
   label.text = s
+
+func reveal_text():
+  animation_player.play("RevealText")
