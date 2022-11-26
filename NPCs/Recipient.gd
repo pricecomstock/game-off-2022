3881dc8627c06ef884abb93c1b81a2ad0e254b98
Price <price.comstock@gmail.com>
2022-11-26 13:46:12 -0500

    seals above recipients!

--- NPCs/Recipient.gd
+++ NPCs/Recipient.gd
@@ -2,9 +2,13 @@ extends StaticBody2D
 
 export var letter_id : int = 1
 onready var delivery_detector: Area2D = $DeliveryDetector
+onready var seal = get_node("%LetterSeal")
 
 func _ready():
   delivery_detector.connect("area_entered", self, "_on_DeliveryDetector_area_entered")
+  var letter = LetterManager.get_letters()[letter_id]
+  seal.set_symbol(letter.symbol)
+  seal.set_color(letter.color)
   pass
 
 func attempt_letter_delivery():
@@ -14,4 +18,4 @@ func attempt_letter_delivery():
     LetterManager.deliver_letter(letter_id)
 
 func _on_DeliveryDetector_area_entered(area:Area2D):
-	attempt_letter_delivery()
+  attempt_letter_delivery()
